9af7704acd9a79440a00ffdf05c2486821affc7b
elvinkosova <elvinkosova@gmail.com>
2024-10-08 06:01:24 +0000

    Update page with new hooks and components

--- app/page.tsx
+++ app/page.tsx
@@ -8,24 +8,43 @@ import {
   Container,
   Content,
   Backdrop,
+  Button,
 } from '@/components/primitives'
 import { PromptForm } from '@/components/composites'
 
-import { useTextGenerator } from '@/hooks'
+import { useTextGenerator, useSpeachGenerator } from '@/hooks'
 
 const Home = () => {
-  const { message, onSubmit } = useTextGenerator()
+  const { message, generateMessage } = useTextGenerator()
+  const { audioUrl, generateAudio, generateAudioPro } = useSpeachGenerator()
+
+  if (audioUrl) {
+    const audio = new Audio(audioUrl)
+    audio.play()
+  }
+
   const defaultMessage =
     'Please enter a topic, select a format, and click button Compose.'
+
+  console.log('Home message:', message)
+  console.log('Home audioUrl:', audioUrl)
 
   return (
     <Backdrop>
       <Container>
         <Heading level={1}>Tech Comedy Central</Heading>
-        <PromptForm onSubmit={onSubmit} />
+        <PromptForm onSubmit={generateMessage} />
         <Content>
-          {message && <Text>{message}</Text>}
           {!message && <Text>{defaultMessage}</Text>}
+          {message && <Text>{message}</Text>}
+          {message && (
+            <Button onClick={() => generateAudio(message)}>🎸 Play</Button>
+          )}
+          {message && (
+            <Button onClick={() => generateAudioPro(message)}>
+              🎸🎸 Play (Pro)
+            </Button>
+          )}
         </Content>
         <Footer>Happy Prompting, Happy Roasting! from Promptlys !</Footer>
         <Image
