381f756da2dd16deb264b90b6079314d4cb92703
elvinkosova <elvinkosova@gmail.com>
2024-10-08 05:27:42 +0000

    Improve primitives

--- components/primitives/Select.tsx
+++ components/primitives/Select.tsx
@@ -26,7 +26,7 @@ interface Props extends ListboxProps {
 const Select = ({ options, placeholder, ...props }: Props, ref: SelectRef) => (
   <Listbox {...props} ref={ref}>
     <div className="relative">
-      <ListboxButton className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-amber-500 sm:text-sm sm:leading-6">
+      <ListboxButton className="relative w-full cursor-pointer rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-amber-500 sm:text-sm sm:leading-6">
         <span className="flex items-center">
           {props.value && (
             <span className="ml-3 block truncate">
@@ -55,7 +55,7 @@ const Select = ({ options, placeholder, ...props }: Props, ref: SelectRef) => (
           <ListboxOption
             key={option.value}
             value={option.value}
-            className="group relative cursor-default select-none py-2 pl-3 pr-9 text-gray-900 data-[focus]:bg-amber-600 data-[focus]:text-white"
+            className="group relative cursor-pointer select-none py-2 pl-3 pr-9 text-gray-900 data-[focus]:bg-amber-600 data-[focus]:text-white"
           >
             <div className="flex items-center">
               <span className="ml-3 block truncate font-normal group-data-[selected]:font-semibold">
